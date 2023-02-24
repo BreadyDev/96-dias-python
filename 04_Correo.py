c707ce64feb3edf4304e67f7074660124999f0ca
BreadyDev <juantonioramos26@gmail.com>
2023-02-23 19:00:25 -0600

    Adicion del dia 4: Emails con python

---
+++ 04_Correo.py
@@ -0,0 +1,18 @@
+import os
+import random
+import smtplib
+
+def enviar_correo(usuario, email):
+    mensaje= (f"{usuario}, esto es un correo automatico usando python")
+    
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    
+    s.login("juantonioramos26@gmail.com", "fjcihvbyzfrjnsmz")
+    s.sendmail("juantonioramos26@gmail.com", email, mensaje)
+    print(f"Mensaje enviado a {usuario}")
+
+correos = [["Antonio Ramos", "20630271@itocotlan.com"], ["Martin", "juanmglezrazo@gmail.com"], ["Danca", "dancarrodoce@gmail.com"], ["Benja", "benjamin3455rg16@gmail.com"]]
+
+for correo in correos:
+    enviar_correo(correo[0], correo[1])
